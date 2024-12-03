c81ce1c97c1db77f0de28e5debe07a9e9d9d6b19
Ilya Tolstenkov <tolstenkov.ilya@mail.ru>
2024-12-03 09:51:08 +0300

    completed tasks

---
+++ main.js
@@ -0,0 +1,123 @@
+// 1
+// let a = prompt("Введите:")
+// if (!isNaN(a)){
+//     if(a % 2 == 0)
+//         console.log("Четное число");
+//     else
+//         console.log("Нечетное число");
+// }
+// else{
+//     b = a.split('')
+//     console.log(b);
+// }
+
+// 2
+// let a = String(prompt("Введите:")).toLowerCase();
+// b = a.split(' ');
+// console.log(b);
+
+// 3
+// let a = prompt("Введите:").trim();
+// if (!isNaN(a)){
+//     console.log("Ошибка ввода");
+// }
+// else{
+//     b = a.length;
+//     console.log(a.toLowerCase() + `(Длина: ${b})`)
+// }
+
+// 4
+// let a = prompt("Введите:").trim();
+// console.log(a.toUpperCase())
+
+// 5
+// let a = prompt("Введите:").trim();
+// if (!isNaN(a)){
+//     console.log("Ошибка ввода");
+// }
+// else{
+//     console.log(a[0].toUpperCase()+a.slice(1, a.length))
+// }
+
+// 6
+// let a = prompt("Введите:").trim();
+// console.log(a.includes(".com") || a.includes(".ru"))
+
+
+// 7
+// let a = prompt("Введите:").trim();
+// console.log(a.endsWith(".com") || a.endsWith(".ru"))
+
+// 8
+// let a = prompt("Введите:").trim();
+// console.log(a.replaceAll('@', '!'))
+
+// 9
+// let a = prompt("Введите:").trim();
+// console.log(a.indexOf('bbb'))
+
+
+// 10
+// let a = prompt("Введите:").trim();
+// console.log(a.lastIndexOf('bbb'))
+
+// 11
+// let a = prompt("Введите:").trim();
+// console.log(a.replaceAll("bbb ", ''))
+
+
+// 12
+// let a = prompt("Введите дату:").trim();
+// year = a.slice(0, 4);
+// month = a. slice(5,7);
+// day = a.slice(8,a.length);
+// console.log(`${day}/${month}/${year}`)
+
+// 13
+// let a = prompt("Введите").trim();
+// console.log(a.toLowerCase().repeat(3));
+
+// 14
+// let a = prompt("Введите").trim();
+// b = a.split(' ');
+// console.log(b.length)
+
+// 14
+// let a = prompt("Введите").trim();
+// b = a.split(' ');
+// console.log(b[0])
+
+// 15
+// let a = prompt("Введите").trim();
+// console.log(a.replaceAll("-", "!"));
+
+// 16
+// let a = prompt("Введите").trim();
+// b = a.split('-');
+// console.log(b.reverse().join('.'));
+
+// 17
+// let a = ['я', 'учу', 'javascript', '!'];
+// console.log(a.join('+'));
+
+// 18
+// let a = prompt("Введите").trim();
+// b = a.split(' ');
+// if(b[0][0] == b[0][0].toUpperCase() && b[1][0] == b[1][0].toUpperCase()){
+//     console.log(true)
+// }
+// else{
+//     console.log(false)
+// }
+
+
+// 19
+// let a = prompt("Введите").trim().toLowerCase();
+// if(a.includes('хорош')){
+//     console.log(true)
+// }
+// else{
+//     console.log(false)
+// }
+
+
